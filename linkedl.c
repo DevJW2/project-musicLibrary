90b0ec42d47e8c2322d7c41c927c3b45d21899da
jensenli049 <jensenli049@gmail.com>
2017-10-19 15:31:51 -0400

    added insert song node

--- linkedl.c
+++ linkedl.c
@@ -2,18 +2,18 @@
 //Project 0: my_tunez
 //Period 10
 /*
-insert nodes in order
-find and return a pointer to a node based on artist and song name
-find and return a pointer to the first song of an artist based on artist name
-Return a pointer to random element in the list.
-remove a single specified node from the list
-free the entire list
+  insert nodes in order
+  find and return a pointer to a node based on artist and song name
+  find and return a pointer to the first song of an artist based on artist name
+  Return a pointer to random element in the list.
+  remove a single specified node from the list
+  free the entire list
 */
 
 
 #include "linkedl.h"
 
-void print_list(struct song_node * list){
+void print_list( song_node * list){
   printf("list content: ");
   
   while(list){
@@ -23,16 +23,56 @@ void print_list(struct song_node * list){
   printf("\n");
 }
 
-struct song_node * insert_front(struct song_node * list, char song_name[], char artist[]){
-  struct song_node * new_node = (struct song_node *)malloc(sizeof(*list));
+song_node * insert_front(  song_node * list, char song_name[], char artist[]){
+  song_node * new_node = (  song_node *)malloc(sizeof(*list));
   (* new_node).next = list;
   (* new_node).name  = song_name;
   (* new_node).artist = artist;
 
   return new_node;
 }
+
+int table_len(){
+  int i = 0;
+  while (table[i++]){}
+  return i;
+}
+
+song_node * table_insert(char song_name[], char artist[], int i){
+  int len = table_len();
+  song_node * temp = (song_node *)malloc(sizeof(song_node));
+  temp->name = song_name;
+  temp->artist = artist;
+  while(len--){
+    if(len == 1){
+      table[len] = table[0];
+      table[0] = temp;
+      break;
+    }
+    table[len+1] = table[len];
+  }
+  return NULL;
+}
+
+song_node * add_song_node(char song_name[], char artist[]){
+  int i = 0;
+  int len = table_len();
+  int cmp = strncmp(table[0]->artist, artist,1);
+  song_node * temp = (song_node *)malloc(sizeof(song_node));
+  while (table[i] && !(cmp<0)){
+    i++;
+    cmp = strncmp(table[i]->artist, artist,1);
+  }
+  if(cmp == 0){
+    return table[i];
+  }
+  else
+    return table_insert(song_name, artist);
+}
+	     
+
 //FIX THIS, THIS DOESN'T DO WHAT I WANT IT TO DO
-struct song_node * compare_songs(struct song_node * list1, struct song_node * list2){
+song_node * compare_songs(  song_node * list1,   song_node * list2){
   //return the song that is literally behind another song in the linked list.
   if (strcmp((* list1).artist, (* list2).artist) < 0){
     return list1;
@@ -52,15 +92,18 @@ struct song_node * compare_songs(struct song_node * list1, struct song_node * li
   return NULL; 
 }
 
-struct song_node * insert_order(struct song_node * list, char song_name[], char artist[]){
-  while(list){
-    list = list->next;
-  }
+song_node * insert_order(  song_node * list, char song_name[], char artist[]){
+  int cmp = strcmp(list->artist, artist);
+  song_node * temp = (song_node *)malloc(sizeof(song_node));
+  
   
 }
 
-struct song_node* free_list(struct song_node * list){
-  struct song_node * tmp; 
+
+
+
+song_node* free_list(  song_node * list){
+  song_node * tmp; 
   while(list){
     tmp = (*list).next;
     free(list);
@@ -75,6 +118,6 @@ struct song_node* free_list(struct song_node * list){
 
 
 int main(){
-  struct song_node * new_list = NULL;
+  song_node * new_list = NULL;
 
 }
--- linkedl.h
+++ linkedl.h
@@ -2,18 +2,20 @@
 #include <stdlib.h>
 #include <string.h> 
 
-struct song_node{
+typedef struct node{
   char name[256];
   char artist[256];
   struct song_node *next;
-};
+} song_node;
 
 struct song_node * table[26];
 
-void print_list(struct song_node *);
-struct song_node * insert_front(struct song_node *, char[], char[]);
-struct song_node * free_list(struct song_node *); 
-struct song_node * insert_order(struct song_node *, char[], char[]);
-struct song_node * compare_songs(struct song_node *, struct song_node *);
+void print_list(song_node *);
+int table_len();
+song_node * table_insert(char song_name[], char artist[], int i);
+song_node * insert_front(song_node *, char[], char[]);
+song_node * free_list(song_node *); 
+song_node * insert_order( song_node *, char[], char[]);
+song_node * checklist(song_node *, struct song_node *);
 
 
