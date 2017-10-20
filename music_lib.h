4ad94773b36eaacd530fa92597f38acd6940cc0d
devjw2 <mcjeffrey168@gmail.com>
2017-10-20 00:31:31 -0400

    changed music_lib.h

--- music_lib.h
+++ music_lib.h
@@ -1,7 +1,5 @@
 #include "linkedl.h"
 
-
-
 song_node* add_song_node(char song_name[], char artist[]);
 
 int table_len();
@@ -9,3 +7,10 @@ int table_len();
 song_node * table_insert(char song_name[], char artist[], int i);
 
 song_node * checklist(song_node *, struct song_node *);
+
+song_node * print_out_letter(char letter[]);
+
+song_node * print_out_song_of_artist(song_node *, char artist[]);
+
+song_node * print_out_library(song_node *);
+
